bef83389df19c5a2ff635670d849c4f6c7354874
paola <paolarocha003@gmail.com>
2026-02-20 23:16:46 -0600

    Destalles 20/02/2026

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
 import forms
-
-from models import db
-from models import Alumnos
-
+from models import db, Alumnos
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
-csrf = CSRFProtect()
-csrf.init_app(app)
+csrf = CSRFProtect(app)
+db.init_app(app)
 
-db.init_app(app)    
 with app.app_context():
     db.create_all()
-
 @app.errorhandler(404)
 def page_not_fount(e):
 	return render_template("404.html"),404
@@ -30,9 +24,36 @@ def index():
     alumno=Alumnos.query.all()
     return render_template("index.html",form=create_form, alumno=alumno)
 
-@app.route("/alumnos")
+@app.route("/alumnos", methods=["GET", "POST"])
 def alumnos():
-	return render_template("Alumnos.html")
+    create_form = forms.UsuarioForm()
+
+    if create_form.validate_on_submit():
+        alum = Alumnos(
+            nombre=create_form.nombre.data,
+            apaterno=create_form.apaterno.data,
+            email=create_form.email.data
+        )
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template("alumnos.html", form=create_form)
+
+@app.route("/detalles", methods=['GET', 'POST'])
+def detalles():
+    create_form=forms.UsuarioForm(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        id=request.args.get('id')
+        nombre=alum1.nombre
+        apaterno=alum1.apaterno
+        email=alum1.email
+    
+    return render_template('detalles.html', id=id, nombre=nombre, apaterno=apaterno, email=email, form=create_form)
+
 
 if __name__ == '__main__':
     app.run()
+    
--- forms.py
+++ forms.py
@@ -1,25 +1,29 @@
-from wtforms import Form
 from flask_wtf import FlaskForm
- 
-from wtforms import StringField, IntegerField
-from wtforms import EmailField
-from wtforms import validators
- 
-class UsuarioForm(Form):
-    id = IntegerField('id', [
-        validators.number_range(min=1, max=20, message='valor no valido')
-    ])
-   
-    nombre = StringField('nombre', [
-        validators.DataRequired(message='El nombre es requerido'),
-        validators.length(min=4, max=20, message='requiere min=4 max=20')
-    ])
-   
-    apaterno = StringField('apaterno', [
-        validators.DataRequired(message='El apellido es requerido')
-    ])
-   
-    email = EmailField('correo', [
-        validators.DataRequired(message='El apellido es requerido'),
-        validators.Email(message='Ingrese un correo valido')
-    ])
+from wtforms import StringField
+from wtforms.fields import EmailField
+from wtforms.validators import DataRequired, Length, Email
+
+class UsuarioForm(FlaskForm):
+
+    nombre = StringField(
+        'Nombre',
+        validators=[
+            DataRequired(message='El nombre es requerido'),
+            Length(min=4, max=20, message='Debe tener entre 4 y 20 caracteres')
+        ]
+    )
+
+    apaterno = StringField(
+        'Apellido',
+        validators=[
+            DataRequired(message='El apellido es requerido')
+        ]
+    )
+
+    email = EmailField(
+        'Correo',
+        validators=[
+            DataRequired(message='El correo es requerido'),
+            Email(message='Ingrese un correo válido')
+        ]
+    )
